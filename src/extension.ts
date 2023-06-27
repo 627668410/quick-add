a30bd29048b640fcb584ac74d80c7837b5b2927a
鹿晶 <=>
2023-06-27 11:30:41 +0800

    更新当前目录路径

--- src/extension.ts
+++ src/extension.ts
@@ -1,32 +1,32 @@
-import * as vscode from "vscode";
-import {writeFileByTemplate} from "./templateFiles";
-import {ConfigItem} from "./types";
-import {messageError} from "./utils";
-const path = require("path");
-const fs = require("fs");
+import * as vscode from 'vscode';
+import {writeFileByTemplate} from './templateFiles';
+import {ConfigItem} from './types';
+import {messageError} from './utils';
+const path = require('path');
+const fs = require('fs');
 
 export function activate(context: vscode.ExtensionContext) {
-  let disposable = vscode.commands.registerCommand("quick-add", (uri) => {
+  let disposable = vscode.commands.registerCommand('quick-add', (uri) => {
     try {
       // 执行命令的项目路径
       const folderPath = vscode.workspace.workspaceFolders?.[0].uri.fsPath;
       if (!folderPath) {
-        messageError("获取项目路径失败");
+        messageError('获取项目路径失败');
         return;
       }
 
       // 执行命令的文件夹路径
-      const basePath = uri._fsPath;
+      const basePath = uri.path;
       if (!basePath) {
-        messageError("获取当前目录路径失败");
+        messageError('获取当前目录路径失败');
         return;
       }
 
       const settings: ConfigItem[] = (vscode.workspace
         .getConfiguration()
-        .get("quick-add") as unknown) as ConfigItem[];
+        .get('quick-add') as unknown) as ConfigItem[];
       if (!settings || !settings.length) {
-        messageError("请添加quick-add配置");
+        messageError('请添加quick-add配置');
         return;
       }
       vscode.window.showQuickPick(settings).then((selection) => {
@@ -52,10 +52,10 @@ export function activate(context: vscode.ExtensionContext) {
               // 这个对象中所有参数都是可选参数
               password: false, // 输入内容是否是密码
               ignoreFocusOut: true, // 默认false，设置为true时鼠标点击别的地方输入框不会消失
-              placeHolder: "请输入新建文件夹名称", // 在输入框内的提示信息
-              prompt: "新建文件夹名称", // 在输入框下方的提示信息
+              placeHolder: '请输入新建文件夹名称', // 在输入框内的提示信息
+              prompt: '新建文件夹名称', // 在输入框下方的提示信息
               validateInput: function (text) {
-                if (text) return "";
+                if (text) return '';
                 return text;
               }, // 对输入内容进行验证并返回
             })
